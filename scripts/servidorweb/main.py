05b510bd16e1b9979e59849f4e9070c32243c0f3
jero98772 <jero98772@protonmail.com>
2021-08-19 08:46:26 -0500

    update tasmision.py and add new web pages to webserver

--- scripts/servidorweb/main.py
+++ scripts/servidorweb/main.py
@@ -3,5 +3,11 @@ app = Flask(__name__)
 @app.route("/")
 	def index():
 		return render_template("index.html")
+@app.route("/"+"pelicula")
+	def qrAndToken():
+		return render_template("QR.html")
+@app.route("/")
+	def manage():
+		return render_template("manage.html")
 if __name__=='__main__':
 	webapp.run(debug=True,host="0.0.0.0",port=9600)
--- scripts/trasmision_datos/trasmision.py
+++ scripts/trasmision_datos/trasmision.py
@@ -4,17 +4,32 @@ class server():
 		self.sock = socket.socket()
 		#self.address = address
 		#self.port = port
-		self.sock.conect((address,port))
-	def sendPause(self):
-	def sendContinue(self):
+		self.sock.bind((address,port))
+		self.conn , self.address =self.sock.accept()
+	def sendPause(self,comand = "pause"):
+		self.conn.send(comand)
+	def sendContinue(self,comand = "continue"):
+		self.conn.send(comand)
 class testing():
 	def __init__(self,address,port,comands = ["pause","continue"]):
 		self.sock = socket.socket()
 		#self.address = address
 		#self.port = port
-		self.sock.conect((address,port))
+		self.sock.connect((address,port))
 	def recive(self):
 		self.sock.listen(1)
 		revs = self.sock.recv(1024)
 		if revs in commands:
-			
+			if revs=="pause":
+				print("send pause to audio")
+			if revs=="continue":
+				print("continue audio")
+def testing():
+	port = 4092
+	address = "localhost"
+	while True:
+	s = server(address,port)
+	t = testing(address,port)
+	s.sendPause()
+	t.recive()
+testing()
